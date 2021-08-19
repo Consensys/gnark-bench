7d89babf3205ba1762c9c65e7fe72d640b94a1e3
Gautam Botrel <gautam.botrel@gmail.com>
2021-08-19 11:35:11 -0500

    feat: added flag that idnicates if we compiled with amd64_adx optim or not

--- cmd/output.go
+++ cmd/output.go
@@ -7,6 +7,9 @@ import (
 	. "github.com/klauspost/cpuid/v2"
 )
 
+// indicates if we compiled the binary with amd64_adx build tag
+var amd64_adx bool
+
 type benchData struct {
 	Backend             string
 	Curve               string
@@ -35,7 +38,7 @@ type benchData struct {
 }
 
 func (bData benchData) headers() []string {
-	return []string{"backend", "curve", "algorithm", "nbConstraints", "nbInternal", "nbSecret", "nbPublic", "nbCoefficients", "ram(mb)", "time(ms)", "throughput(constraints/s)", "througputPerCore(constraints/s)", "nbPhysicalCores", "nbThreadsPerCore", "nbLogicalCores", "cacheLine", "l1d", "l1i", "l2", "l3", "freq", "adx", "cpu"}
+	return []string{"backend", "curve", "algorithm", "nbConstraints", "nbInternal", "nbSecret", "nbPublic", "nbCoefficients", "ram(mb)", "time(ms)", "throughput(constraints/s)", "througputPerCore(constraints/s)", "nbPhysicalCores", "nbThreadsPerCore", "nbLogicalCores", "cacheLine", "l1d", "l1i", "l2", "l3", "freq", "adx", "cpu", "amd64_adx"}
 }
 func (bData benchData) values() []string {
 
@@ -64,5 +67,6 @@ func (bData benchData) values() []string {
 		strconv.Itoa(int(CPU.Hz / 1000000)),
 		fmt.Sprintf("%v", CPU.Supports(ADX) && CPU.Supports(BMI2)),
 		CPU.BrandName,
+		fmt.Sprintf("%v", amd64_adx),
 	}
 }
